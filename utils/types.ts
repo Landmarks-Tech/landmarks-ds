6ff225a041b628754cf5772fcd72d62a278ea94a
ancashoria <horia.ancas@gmail.com>
2022-01-03 12:02:49 +0200

    removed Range because it was breaking the build

--- utils/types.ts
+++ utils/types.ts
@@ -1,12 +1,5 @@
 import { Atoms } from 'site/styles/sprinkles.css'
 
-// reccursively go through all objects until given range
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#recursive-conditional-types
-export type Range<T extends number> = number extends T ? number : _Range<T, []>
-type _Range<T extends number, R extends unknown[]> = R['length'] extends T
-  ? R[number]
-  : _Range<T, [R['length'], ...R]>
-
 export interface IUIComponent extends Atoms {
   style?: any
   className?: string
